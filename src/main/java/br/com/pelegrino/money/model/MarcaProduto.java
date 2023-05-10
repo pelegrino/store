17ce96fca57d30886f1bbb206c43434a153096e1
Pelegrino <pelegrino@gmail.com>
2023-05-10 15:09:26 -0300

    Classe Categoria Produto

--- src/main/java/br/com/pelegrino/money/model/MarcaProduto.java
+++ src/main/java/br/com/pelegrino/money/model/MarcaProduto.java
@@ -2,6 +2,7 @@ package br.com.pelegrino.money.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +20,8 @@ public class MarcaProduto implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
 	private Long id;
+	
+	@Column(nullable = false)
 	private String nomeDesc;
 
 	public Long getId() {
