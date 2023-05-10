513308c59a810646892c70642de091d4fa0d9565
Pelegrino <pelegrino@gmail.com>
2023-05-10 15:37:03 -0300

    Classe Pessoa Física

--- src/main/java/br/com/pelegrino/money/model/Pessoa.java
+++ src/main/java/br/com/pelegrino/money/model/Pessoa.java
@@ -1,6 +1,7 @@
 package br.com.pelegrino.money.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -27,4 +28,53 @@ public abstract class Pessoa implements Serializable {
 	
 	private String telefone;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(id, other.id);
+	}
+	
 }
