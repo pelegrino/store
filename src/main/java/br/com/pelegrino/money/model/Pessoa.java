26bca38f20d4d19707cc95a066e4d74ee9c2468b
Pelegrino <pelegrino@gmail.com>
2023-05-10 15:30:20 -0300

    Classe Pessoa

---
+++ src/main/java/br/com/pelegrino/money/model/Pessoa.java
@@ -0,0 +1,30 @@
+package br.com.pelegrino.money.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@SequenceGenerator(name = "seq_pessoa", sequenceName = "seq_pessoa", initialValue = 1, allocationSize = 1)
+public abstract class Pessoa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_pessoa")
+	private Long id;
+	
+	private String nome;
+	
+	private String email;
+	
+	private String telefone;
+
+}
