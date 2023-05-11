0cfec90487f882ee1acd576a65c621071c3220bf
Pelegrino <pelegrino@gmail.com>
2023-05-11 15:30:11 -0300

    Arquivos SQL

--- src/main/java/br/com/pelegrino/money/model/Usuario.java
+++ src/main/java/br/com/pelegrino/money/model/Usuario.java
@@ -41,19 +41,23 @@ public class Usuario implements UserDetails {
 	@Temporal(TemporalType.DATE)
 	private Date dataAtualSenha;
 	
+	
 	@OneToMany(fetch = FetchType.LAZY)
 	@JoinTable(name = "usuarios_acessos", 
-				uniqueConstraints = @UniqueConstraint(columnNames = {"usuario_id", "acesso_id"}, name = "unique_acesso_user"),
+				uniqueConstraints = @UniqueConstraint(columnNames = {"usuario_id", "acesso_id"}, 
+														name = "unique_acesso_user"),
 				joinColumns = @JoinColumn(name = "usuario_id", 
 											referencedColumnName = "id", 
 											table = "usuario", 
 											unique = false, 
-											foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)),
+											foreignKey = @ForeignKey(name = "usuario_fk", 
+																		value = ConstraintMode.CONSTRAINT)),
 				inverseJoinColumns = @JoinColumn(name = "acesso_id", 
 													unique = false, 
 													referencedColumnName = "id", 
 													table = "acesso",
-													foreignKey = @ForeignKey(name = "acesso_fk", value = ConstraintMode.CONSTRAINT)))
+													foreignKey = @ForeignKey(name = "acesso_fk", 
+																				value = ConstraintMode.CONSTRAINT)))
 	private List<Acesso> acessos;
 	
 	
---
+++ src/main/java/db/migration/V1_11-05-2023.sql
@@ -0,0 +1,5 @@
+select constraint_name from information_schema.constraint_column_usage 
+ where table_name = 'usuarios_acessos' and column_name = 'acesso_id'
+ and constraint_name <> 'unique_acesso_user';
+ 
+ alter table usuarios_acessos drop constraint "uk_qs91qokws6i46m1vnsoakivh1";
