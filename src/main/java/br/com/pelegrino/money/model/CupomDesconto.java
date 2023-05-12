a487c8e4a806dbcefea4e36589a08c5960dfe121
Pelegrino <pelegrino@gmail.com>
2023-05-12 13:17:59 -0300

    Classe Cupom Desconto

---
+++ src/main/java/br/com/pelegrino/money/model/CupomDesconto.java
@@ -0,0 +1,94 @@
+package br.com.pelegrino.money.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "cupom_desconto")
+@SequenceGenerator(name = "seq_cupom_desconto", sequenceName = "seq_cupom_desconto", allocationSize = 1, initialValue = 1)
+public class CupomDesconto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_cupom_desconto")
+	private Long id;
+	
+	private String codDescricao;
+	
+	private BigDecimal valorRealDesc;
+	
+	private BigDecimal valorPorcentDesc;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataValidade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCodDescricao() {
+		return codDescricao;
+	}
+
+	public void setCodDescricao(String codDescricao) {
+		this.codDescricao = codDescricao;
+	}
+
+	public BigDecimal getValorRealDesc() {
+		return valorRealDesc;
+	}
+
+	public void setValorRealDesc(BigDecimal valorRealDesc) {
+		this.valorRealDesc = valorRealDesc;
+	}
+
+	public BigDecimal getValorPorcentDesc() {
+		return valorPorcentDesc;
+	}
+
+	public void setValorPorcentDesc(BigDecimal valorPorcentDesc) {
+		this.valorPorcentDesc = valorPorcentDesc;
+	}
+
+	public Date getDataValidade() {
+		return dataValidade;
+	}
+
+	public void setDataValidade(Date dataValidade) {
+		this.dataValidade = dataValidade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CupomDesconto other = (CupomDesconto) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
