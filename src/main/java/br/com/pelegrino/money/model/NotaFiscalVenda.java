47be0c04fefc0b44a3e6715e9b1bd655bbe188da
Pelegrino <pelegrino@gmail.com>
2023-05-13 11:15:22 -0300

    Classe Venda Compra Loja Virtual

--- src/main/java/br/com/pelegrino/money/model/NotaFiscalVenda.java
+++ src/main/java/br/com/pelegrino/money/model/NotaFiscalVenda.java
@@ -4,10 +4,14 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
@@ -34,6 +38,18 @@ public class NotaFiscalVenda implements Serializable {
 	@Column(columnDefinition = "text")
 	private String pdf;
 	
+	@OneToOne
+	@JoinColumn(name = "venda_compra_loja_virtual_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "venda_compra_loja_virtual_fk"))
+	private VendaCompraLojaVirtual vendaCompraLojaVirtual;
+	
+	
+	public void setVendaCompraLojaVirtual(VendaCompraLojaVirtual vendaCompraLojaVirtual) {
+		this.vendaCompraLojaVirtual = vendaCompraLojaVirtual;
+	}
+	
+	public VendaCompraLojaVirtual getVendaCompraLojaVirtual() {
+		return vendaCompraLojaVirtual;
+	}
 
 	public Long getId() {
 		return id;
--- src/main/java/br/com/pelegrino/money/model/StatusRastreio.java
+++ src/main/java/br/com/pelegrino/money/model/StatusRastreio.java
@@ -3,10 +3,14 @@ package br.com.pelegrino.money.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
@@ -29,7 +33,18 @@ public class StatusRastreio implements Serializable {
 	
 	private String status;
 	
-
+	@ManyToOne
+	@JoinColumn(name = "venda_compra_loja_virtual_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "venda_compra_loja_virtual_fk"))
+	private VendaCompraLojaVirtual vendaCompraLojaVirtual;
+	
+	public VendaCompraLojaVirtual getVendaCompraLojaVirtual() {
+		return vendaCompraLojaVirtual;
+	}
+	
+	public void setVendaCompraLojaVirtual(VendaCompraLojaVirtual vendaCompraLojaVirtual) {
+		this.vendaCompraLojaVirtual = vendaCompraLojaVirtual;
+	}
+	
 	public Long getId() {
 		return id;
 	}
