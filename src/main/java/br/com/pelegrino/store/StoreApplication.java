e05d38713ec0a2269e73f127a4036261ae337539
Pelegrino <pelegrino@gmail.com>
2023-05-17 09:17:35 -0300

    Anotações da Classe Application do Spring Boot

--- src/main/java/br/com/pelegrino/store/StoreApplication.java
+++ src/main/java/br/com/pelegrino/store/StoreApplication.java
@@ -3,9 +3,15 @@ package br.com.pelegrino.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EntityScan(basePackages = "br.com.pelegrino.store.model")
+@ComponentScan(basePackages = {"br.*"})
+@EnableJpaRepositories(basePackages = {"br.com.pelegrino.store.repository"})
+@EnableTransactionManagement
 public class StoreApplication {
 
 	public static void main(String[] args) {
