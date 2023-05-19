884d09e5f9275cf8c6fffd246b1210c908fe2cf9
Pelegrino <pelegrino@gmail.com>
2023-05-19 15:11:18 -0300

    Teste com Postman

--- src/main/java/br/com/pelegrino/store/controller/AcessoController.java
+++ src/main/java/br/com/pelegrino/store/controller/AcessoController.java
@@ -1,22 +1,44 @@
 package br.com.pelegrino.store.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.pelegrino.store.model.Acesso;
+import br.com.pelegrino.store.repository.AcessoRepository;
 import br.com.pelegrino.store.service.AcessoService;
 
 @Controller
+@RestController
 public class AcessoController {
 
 	@Autowired
 	private AcessoService acessoService;
 	
-	@PostMapping("/salvarAcesso")
-	public Acesso salvarAcesso(Acesso acesso) {
+	@Autowired
+	private AcessoRepository acessoRepository;
+	
+	@ResponseBody
+	@PostMapping(value = "/salvarAcesso")
+	public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso) {
 		
-		return acessoService.save(acesso);
+		Acesso acessoSalvo = acessoService.save(acesso);
+		
+		return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
+	}
+	
+	@ResponseBody
+	@PostMapping(value = "/deleteAcesso")
+	public ResponseEntity<?> deleteAcesso(@RequestBody Acesso acesso) {
+		
+		acessoRepository.deleteById(acesso.getId());
+		
+		return new ResponseEntity(HttpStatus.OK);
 	}
 	
 }
