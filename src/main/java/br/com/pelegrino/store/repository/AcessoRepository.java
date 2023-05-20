cb752f49884d4bbf84a7f4d6a6f7c057022f63ea
Pelegrino <pelegrino@gmail.com>
2023-05-20 09:23:24 -0300

    Testes com Junit

--- src/main/java/br/com/pelegrino/store/repository/AcessoRepository.java
+++ src/main/java/br/com/pelegrino/store/repository/AcessoRepository.java
@@ -1,6 +1,9 @@
 package br.com.pelegrino.store.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.pelegrino.store.model.Acesso;
@@ -8,5 +11,8 @@ import br.com.pelegrino.store.model.Acesso;
 
 @Transactional
 public interface AcessoRepository extends JpaRepository<Acesso, Long> {
+	
+	@Query("select a from Acesso a where upper(trim(a.descricao)) like %?1%")
+	List<Acesso> buscaAcessoDesc(String desc);
 
 }
--- src/test/java/br/com/pelegrino/store/StoreApplicationTests.java
+++ src/test/java/br/com/pelegrino/store/StoreApplicationTests.java
@@ -1,20 +1,24 @@
 package br.com.pelegrino.store;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import br.com.pelegrino.store.controller.AcessoController;
 import br.com.pelegrino.store.model.Acesso;
+import br.com.pelegrino.store.repository.AcessoRepository;
+import junit.framework.TestCase;
 
 @SpringBootTest(classes = StoreApplication.class)
-class StoreApplicationTests {
+class StoreApplicationTests extends TestCase {
 	
 	//@Autowired
 	//private AcessoService acessoService;
 
-	//@Autowired
-	//private AcessoRepository acessoRepository;
+	@Autowired
+	private AcessoRepository acessoRepository;
 	
 	@Autowired
 	private AcessoController acessoController;
@@ -25,7 +29,37 @@ class StoreApplicationTests {
 		Acesso acesso = new Acesso();
 		
 		acesso.setDescricao("ROLE_ADMIN");
-		acessoController.salvarAcesso(acesso);
+		
+		//Grava no BD
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+		
+		assertEquals(true, acesso.getId() > 0);
+		
+		//Valida dados
+		assertEquals("ROLE_ADMIN", acesso.getDescricao());
+		
+		//Teste de carregamento
+		Acesso acesso2 = acessoRepository.findById(acesso.getId()).get();
+		
+		assertEquals(acesso.getId(), acesso2.getId());
+		
+		//Teste de delete
+		acessoRepository.deleteById(acesso2.getId());
+		acessoRepository.flush();
+	
+		Acesso acesso3 = acessoRepository.findById(acesso2.getId()).orElse(null);
+		
+		assertEquals(true, acesso3 == null);
+		
+		//Teste de Query
+		acesso = new Acesso();
+		acesso.setDescricao("ROLE_ALUNO");
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+		List<Acesso> acessos = acessoRepository.buscaAcessoDesc("ALUNO".trim().toUpperCase());
+		
+		assertEquals(1, acessos.size());
+		
+		acessoRepository.deleteById(acesso.getId());
 	}
 
 }
