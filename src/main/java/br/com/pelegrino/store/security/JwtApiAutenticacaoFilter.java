13269cf8ef2146408c8fa7e978078ba714d817f7
Pelegrino <pelegrino@gmail.com>
2023-05-24 11:23:14 -0300

    Criando classe JWT Api Autenticação Filter

---
+++ src/main/java/br/com/pelegrino/store/security/JwtApiAutenticacaoFilter.java
@@ -0,0 +1,33 @@
+package br.com.pelegrino.store.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+//Filtro onde todas as requisições são capturadas
+public class JwtApiAutenticacaoFilter extends GenericFilterBean {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		//Autenticação do usuário
+		Authentication authentication = new JWTTokenAutenticacaoService()
+								.getAuthentication((HttpServletRequest) request, (HttpServletResponse) response);
+		
+		//Coloca o processo de autenticação para o Spring Security
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		chain.doFilter(request, response);
+		
+	}
+
+}
