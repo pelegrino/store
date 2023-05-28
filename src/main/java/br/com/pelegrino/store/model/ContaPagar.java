f93410551d5da07497ca0e087cb525adea7f8271
Pelegrino <pelegrino@gmail.com>
2023-05-28 10:26:17 -0300

    Dividir os dados da tabela por empresa

--- src/main/java/br/com/pelegrino/store/model/ContaPagar.java
+++ src/main/java/br/com/pelegrino/store/model/ContaPagar.java
@@ -64,6 +64,17 @@ public class ContaPagar implements Serializable {
 				foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_forn_fk"))
 	private Pessoa pessoa_fornecedor;
 	
+	@ManyToOne(targetEntity = Pessoa.class)
+	@JoinColumn(name = "empresa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "empresa_id_fk"))
+	private Pessoa empresa;
+	
+	public Pessoa getEmpresa() {
+		return empresa;
+	}
+	
+	public void setEmpresa(Pessoa empresa) {
+		this.empresa = empresa;
+	}
 
 	public Pessoa getPessoa_fornecedor() {
 		return pessoa_fornecedor;
