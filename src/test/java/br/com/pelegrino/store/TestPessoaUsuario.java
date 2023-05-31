c7ba1111fdb4235e55d595f986646a832eb0c6a4
Pelegrino <pelegrino@gmail.com>
2023-05-31 13:54:18 -0300

    Teste Unitário de Endereços de entrega e cobrança

--- src/test/java/br/com/pelegrino/store/TestPessoaUsuario.java
+++ src/test/java/br/com/pelegrino/store/TestPessoaUsuario.java
@@ -1,6 +1,7 @@
 package br.com.pelegrino.store;
 
 import java.util.Calendar;
+import java.util.Iterator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 
 import br.com.pelegrino.store.controller.PessoaController;
+import br.com.pelegrino.store.enums.TipoEndereco;
+import br.com.pelegrino.store.model.Endereco;
 import br.com.pelegrino.store.model.PessoaJuridica;
 import junit.framework.TestCase;
 
@@ -32,7 +35,42 @@ public class TestPessoaUsuario extends TestCase {
 		pessoaJuridica.setNomeFantasia("Pelegrino");
 		pessoaJuridica.setRazaoSocial("Viagem e Turismo");
 		
+		Endereco endereco1 = new Endereco();
+		endereco1.setBairro("Sta. Clara");
+		endereco1.setCep("17222-888");
+		endereco1.setComplemento("Casa");
+		endereco1.setEmpresa(pessoaJuridica);
+		endereco1.setNumero("889");
+		endereco1.setPessoa(pessoaJuridica);
+		endereco1.setRuaLogradouro("Av. Tamoios");
+		endereco1.setTipoEndereco(TipoEndereco.COBRANCA);
+		endereco1.setUf("SP");
+		endereco1.setCidade("Tupã");
+		
+		Endereco endereco2 = new Endereco();
+		endereco2.setBairro("Centro");
+		endereco2.setCep("17222-998");
+		endereco2.setComplemento("Apartametno");
+		endereco2.setEmpresa(pessoaJuridica);
+		endereco2.setNumero("556");
+		endereco2.setPessoa(pessoaJuridica);
+		endereco2.setRuaLogradouro("Av. Tabajaras");
+		endereco2.setTipoEndereco(TipoEndereco.ENTREGA);
+		endereco2.setUf("SP");
+		endereco2.setCidade("Tupã");
+		
+		pessoaJuridica.getEnderecos().add(endereco1);
+		pessoaJuridica.getEnderecos().add(endereco2);
+		
 		pessoaController.salvarPj(pessoaJuridica);
+		
+		assertEquals(true, pessoaJuridica.getId() > 0);
+		
+		for (Endereco endereco : pessoaJuridica.getEnderecos()) {
+			assertEquals(true, endereco.getId() > 0);
+		}
+		
+		assertEquals(2, pessoaJuridica.getEnderecos().size());
 		
 	}
 	
