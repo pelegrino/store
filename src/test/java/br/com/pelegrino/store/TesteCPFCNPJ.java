34c3f38893e985db2ea470d15edf4dd6617ed06e
Pelegrino <pelegrino@gmail.com>
2023-06-04 10:57:55 -0300

    Classe de Validação de CPF

--- src/test/java/br/com/pelegrino/store/TesteCPFCNPJ.java
+++ src/test/java/br/com/pelegrino/store/TesteCPFCNPJ.java
@@ -1,13 +1,16 @@
 package br.com.pelegrino.store;
 
 import br.com.pelegrino.store.util.ValidaCNPJ;
+import br.com.pelegrino.store.util.ValidaCPF;
 
 public class TesteCPFCNPJ {
 	
 	public static void main(String[] args) {
-		boolean isCnpj = ValidaCNPJ.isCNPJ("43.610.502/0001-47");
-		
+		boolean isCnpj = ValidaCNPJ.isCNPJ("43.610.502/0001-46");
 		System.out.println("CNPJ válido: " + isCnpj);
+
+		boolean isCpf = ValidaCPF.isCPF("271.975.648-23");
+		System.out.println("CPF válido: " + isCpf);
 	}
 
 }
