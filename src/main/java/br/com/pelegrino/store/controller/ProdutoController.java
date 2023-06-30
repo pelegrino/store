47c0f039f7346289d07fcfcd3d5fdd910a4a5d89
Pelegrino <pelegrino@gmail.com>
2023-06-30 15:29:30 -0300

    Validações e envio de email na classe produtos

--- src/main/java/br/com/pelegrino/store/controller/ProdutoController.java
+++ src/main/java/br/com/pelegrino/store/controller/ProdutoController.java
@@ -1,7 +1,9 @@
 package br.com.pelegrino.store.controller;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
+import javax.mail.MessagingException;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.pelegrino.store.ExceptionStore;
 import br.com.pelegrino.store.model.Produto;
 import br.com.pelegrino.store.repository.ProdutoRepository;
+import br.com.pelegrino.store.service.ServiceSendEmail;
 
 @Controller
 @RestController
@@ -27,9 +30,20 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
+	@Autowired
+	private ServiceSendEmail serviceSendEmail;
+	
 	@ResponseBody
 	@PostMapping(value = "/salvarProduto")
-	public ResponseEntity<Produto> salvarProduto(@RequestBody @Valid Produto produto) throws ExceptionStore {
+	public ResponseEntity<Produto> salvarProduto(@RequestBody @Valid Produto produto) throws ExceptionStore, UnsupportedEncodingException, MessagingException {
+		
+		if (produto.getTipoUnidade() == null || produto.getTipoUnidade().trim().isEmpty()) {
+			throw new ExceptionStore("Tipo da unidade deve ser informada.");
+		}
+		
+		if (produto.getNome().length() < 10) {
+			throw new ExceptionStore("Nome do produto deve ter mais de 10 letras.");
+		}
 		
 		if (produto.getEmpresa() == null || produto.getEmpresa().getId() <= 0) {
 			throw new ExceptionStore("Empresa responsável deve ser informada.");
@@ -53,7 +67,24 @@ public class ProdutoController {
 			throw new ExceptionStore("Marca deve ser informada.");
 		}
 		
+		if (produto.getQtdEstoque() < 1) {
+			throw new ExceptionStore("O produto deve ter no mínimo 1 no estoque.");
+		}
+		
 		Produto produtoSalvo = produtoRepository.save(produto);
+		
+		if (produto.getAlertaQtdEstoque() && produto.getQtdEstoque() <= 1) {
+			
+			StringBuilder html = new StringBuilder();
+			html.append("<h2>")
+			.append("Produto: " + produto.getNome())
+			.append(" com estoque baixo: " + produto.getQtdEstoque());
+			html.append("<p> Id Prod.: ").append(produto.getId()).append("</h2>");
+			
+			if (produto.getEmpresa().getEmail() != null) {
+				serviceSendEmail.enviarEmailHtml("Produto sem estoque", html.toString(), produto.getEmpresa().getEmail());
+			}
+		}
 		
 		return new ResponseEntity<Produto>(produtoSalvo, HttpStatus.OK);
 	}
