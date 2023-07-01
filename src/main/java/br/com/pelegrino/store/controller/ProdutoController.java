3ac3632d20743ed8e1500dc37fdb3184639cc38d
Pelegrino <pelegrino@gmail.com>
2023-07-01 10:49:12 -0300

    Crud de Produto (imagem)

--- src/main/java/br/com/pelegrino/store/controller/ProdutoController.java
+++ src/main/java/br/com/pelegrino/store/controller/ProdutoController.java
@@ -1,10 +1,16 @@
 package br.com.pelegrino.store.controller;
 
-import java.io.UnsupportedEncodingException;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.mail.MessagingException;
 import javax.validation.Valid;
+import javax.xml.bind.DatatypeConverter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -35,7 +41,7 @@ public class ProdutoController {
 	
 	@ResponseBody
 	@PostMapping(value = "/salvarProduto")
-	public ResponseEntity<Produto> salvarProduto(@RequestBody @Valid Produto produto) throws ExceptionStore, UnsupportedEncodingException, MessagingException {
+	public ResponseEntity<Produto> salvarProduto(@RequestBody @Valid Produto produto) throws ExceptionStore, MessagingException, IOException {
 		
 		if (produto.getTipoUnidade() == null || produto.getTipoUnidade().trim().isEmpty()) {
 			throw new ExceptionStore("Tipo da unidade deve ser informada.");
@@ -69,6 +75,63 @@ public class ProdutoController {
 		
 		if (produto.getQtdEstoque() < 1) {
 			throw new ExceptionStore("O produto deve ter no mínimo 1 no estoque.");
+		}
+		
+		if (produto.getImagens() == null || produto.getImagens().isEmpty() || produto.getImagens().size() == 0) {
+			throw new ExceptionStore("Deve ser informado imagens para o produto.");
+		}
+		
+		if (produto.getImagens().size() < 3) {
+			throw new ExceptionStore("Deve ser informado pelo menos 3 imagens do produto.");
+		}
+		
+		if (produto.getImagens().size() > 6) {
+			throw new ExceptionStore("Deve ser informado no máximo 6 imagens do produto.");
+		}
+		
+		if (produto.getId() == null) {
+			
+			for (int x = 0; x < produto.getImagens().size(); x++) {
+				produto.getImagens().get(x).setProduto(produto);
+				produto.getImagens().get(x).setEmpresa(produto.getEmpresa());
+				
+				String base64Image = "";
+			
+				if (produto.getImagens().get(x).getImagemOriginal().contains("data:image")) {
+					base64Image = produto.getImagens().get(x).getImagemOriginal().split(",")[1];
+					
+				} else {
+					base64Image = produto.getImagens().get(x).getImagemOriginal();
+				}
+				
+				byte[] imagemBytes = DatatypeConverter.parseBase64Binary(base64Image);
+				
+				BufferedImage  bufferedImage = ImageIO.read(new ByteArrayInputStream(imagemBytes));
+				
+				if (bufferedImage != null) {
+					
+					int type = bufferedImage.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : bufferedImage.getType();
+					int largura = Integer.parseInt("800");
+					int altura = Integer.parseInt("600");
+					
+					BufferedImage resizedImage = new BufferedImage(largura, altura, type);
+					Graphics2D g = resizedImage.createGraphics();
+					g.drawImage(bufferedImage, 0, 0, largura, altura, null);
+					g.dispose();
+					
+					ByteArrayOutputStream baos = new ByteArrayOutputStream();
+					ImageIO.write(resizedImage, "png", baos);
+					
+					String miniImgBase64 = "data:image/png;base64," + DatatypeConverter.printBase64Binary(baos.toByteArray());
+					
+					produto.getImagens().get(x).setImagemMiniatura(miniImgBase64);
+					
+					bufferedImage.flush();
+					resizedImage.flush();
+					baos.flush();
+					baos.close();
+				}
+			}
 		}
 		
 		Produto produtoSalvo = produtoRepository.save(produto);
