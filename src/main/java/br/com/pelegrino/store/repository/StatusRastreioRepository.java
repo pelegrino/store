79af15357435d3cd5cac817b531018a2f18d8ba4
Pelegrino <pelegrino@gmail.com>
2023-07-11 17:01:52 -0300

    Criando endpoint de Status de Rastreio

--- src/main/java/br/com/pelegrino/store/repository/StatusRastreioRepository.java
+++ src/main/java/br/com/pelegrino/store/repository/StatusRastreioRepository.java
@@ -1,11 +1,17 @@
 package br.com.pelegrino.store.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.pelegrino.store.model.StatusRastreio;
 
 @Transactional
 public interface StatusRastreioRepository extends JpaRepository<StatusRastreio, Long> {
+	
+	@Query(value = "select s from StatusRastreio s where s.vendaCompraLojaVirtual.id = ?1 order by s.id")
+	public List<StatusRastreio> listaRastreioVenda(Long idVenda);
 
 }
